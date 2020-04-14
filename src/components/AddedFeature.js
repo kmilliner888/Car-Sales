4adbb32c5b9ae81033f569be5c3ff196028d289d
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-04-14 00:15:41 -0400

    setup Redux and reducer, connected components, added actions

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
+import { removeFeature } from '../actions/actions';
+
+import { connect } from 'react-redux';
+
 const AddedFeature = props => {
+  console.log("AddedFeature props", props);
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={()=> props.removeFeature(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = state => {
+  return {
+    
+  };
+};
+
+export default connect(mapStateToProps, {removeFeature})(AddedFeature);
