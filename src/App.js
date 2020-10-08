5811edc7e573d413f4974d37908336671f574f75
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-10-08 01:20:41 -0400

    update sass binding, remove commented out code

--- src/App.js
+++ src/App.js
@@ -12,13 +12,7 @@ import Total from './components/Total';
 const App = (props) => {
   console.log("App props", props);
 
-  // const removeFeature = item => {
-  //   // dispatch an action here to remove an item
-  // };
 
-  // const buyItem = item => {
-  //   // dipsatch an action here to add an item
-  // };
 
   return (
     <div className="boxes">
